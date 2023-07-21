8d5c5ec58f5ce5282f82900b6c3f7c0214e16261
Owatron <touseef@outlook.in>
2023-07-21 22:02:00 +0530

    Create tlds.go

---
+++ tlds.go
@@ -0,0 +1,12 @@
+package parser
+
+import (
+	"index/suffixarray"
+	"strings"
+)
+
+func CreateTLDIndex(tlds []string) *suffixarray.Index {	
+	data := []byte("\x00" + strings.Join(tlds, "\x00") + "\x00")
+	sa := suffixarray.New(data)
+	return sa
+}
